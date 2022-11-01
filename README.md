49e26f49bf62127433a212db3cf8c4312c5fc395
MelvDouc <melv.douc@gmaiL.com>
2022-11-02 00:47:53 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vanilla JS SPA Router
+
+Note that this app requires a server that redirects all requests to index.html in order to work.
---
+++ main.js
@@ -0,0 +1,50 @@
+const mainElement = document.querySelector("main");
+const pageNotFoundMessage = "<h1>Page not found</h1><a href='/'>Home</a>";
+
+const routes = new Map()
+  .set(/^\/(index\.html)?$/, {
+    title: "Home",
+    template: "home.html"
+  })
+  .set("/about", {
+    title: "About",
+    template: "about.html"
+  });
+
+// Run on page load  
+await handleLocation();
+// Run on prev/next page navigator button click
+window.addEventListener("popstate", handleLocation);
+// Run on anchor click
+document.querySelectorAll("header a").forEach((a) => {
+  a.addEventListener("click", async (e) => {
+    // Prevent navigation
+    e.preventDefault();
+    // Update url
+    window.history.pushState({}, "", a.href);
+    // Update page content
+    await handleLocation();
+  });
+});
+
+async function handleLocation() {
+  const path = location.pathname + location.search;
+
+  for (const key of routes.keys()) {
+    if (key instanceof RegExp && key.test(path) || key === path) {
+      const { title, template } = routes.get(key);
+      document.title = title;
+      const html = await fetch(`/templates/${template}`)
+        .then((res) => res.text())
+        .catch((err) => {
+          console.log(err);
+          return pageNotFoundMessage;
+        });
+      mainElement.innerHTML = html;
+      return;
+    }
+  }
+
+  document.title = "Page not found";
+  mainElement.innerHTML = pageNotFoundMessage;
+}
